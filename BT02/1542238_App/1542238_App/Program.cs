46c9a69a0710603f6aaf60f434e4fc714a871780
DESKTOP-LK3LGN2\ng.phuocloc <phuocloc@gmail.com>
2017-04-09 17:39:54 +0700

    BT_02 Import Data

---
+++ BT02/1542238_App/1542238_App/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace _1542238_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pkgLocation = @"C:\Users\ng.phuocloc\Documents\24HCB-PTHTTTHD\BT02\1542238\1542238\Import.dtsx";
+
+            Package pkg;
+            Application app;
+            DTSExecResult pkgResults;
+            Variables vars;
+
+            //string variableName = string.Empty;
+            //variableName = "User::FolderPath";
+
+
+            app = new Application();
+            pkg = app.LoadPackage(pkgLocation, null);
+
+            vars = pkg.Variables;
+            string filePath = @"C:\Users\ng.phuocloc\Documents\24HCB-PTHTTTHD\BT02\1542238\COW_INFO\Cow_info1.xls";
+            vars["User::file_path"].Value = "C:\\Users\\ng.phuocloc\\Documents\\24HCB-PTHTTTHD\\BT02\\1542238\\COW_INFO\\Cow_info1.xls"; ;
+            //var file = pkg.Connections.Add("EXCEL");
+            //file.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source= C:\Users\ng.phuocloc\Documents\24HCB-PTHTTTHD\BT02\1542238\COW_INFO\Cow_info1.xls;Extended Properties='EXCEL 8.0; HDR = YES';";
+
+            pkgResults = pkg.Execute(null, vars, null, null, null);
+
+            if (pkgResults == DTSExecResult.Success)
+                Console.WriteLine("Package ran successfully");
+            else
+                Console.WriteLine("Package failed");
+        }
+    }
+}
---
+++ BT02/1542238_App/1542238_Form/Form1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SqlServer.Dts.Runtime;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _1542238_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string pkgLocation = @"C:\Users\ng.phuocloc\Documents\24HCB-PTHTTTHD\BT02\1542238\1542238\Import.dtsx";
+
+                    Package pkg;
+                    Microsoft.SqlServer.Dts.Runtime.Application app;
+                    DTSExecResult pkgResults;
+                    Variables vars;
+
+                    app = new Microsoft.SqlServer.Dts.Runtime.Application();
+                    pkg = app.LoadPackage(pkgLocation, null);
+                    textBox1.Text = file;
+
+                    vars = pkg.Variables;
+                    vars["User::file_path"].Value = file;
+
+                    pkgResults = pkg.Execute(null, vars, null, null, null);
+
+                    if (pkgResults == DTSExecResult.Success)
+                        MessageBox.Show("Import successfully");
+                    else
+                        MessageBox.Show("Import failed");
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+    }
+}
